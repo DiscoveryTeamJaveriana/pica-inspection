41c3975b8b00adedc0691ebec096fff017e81163
DxMortem <die.9511@gmail.com>
2021-11-02 22:39:52 -0500

    First Commit

---
+++ src/main/java/co/edu/javeriana/discovery/pica/inspection/PicaInspectionApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.discovery.pica.inspection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicaInspectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicaInspectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javeriana/discovery/pica/inspection/PicaInspectionApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.discovery.pica.inspection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicaInspectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
